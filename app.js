a738a50c5867ff6c1c16af86131918758c74319c
tohidshabanloo <tshabanloo@hotmail.com>
2022-03-04 12:50:01 +0330

    2

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const reset = document.querySelector(".reset");
 send.addEventListener("click", () =>{
     
 
-    const name = document.querySelector(".name").value;
+    const name_value = document.querySelector(".name").value;
     localStorage.setItem("name_value", name_value);
     
-    const email = document.querySelector(".email").value;;
+    const email_value = document.querySelector(".email").value;;
     localStorage.setItem("email_value", email_value);
     
-    const message = document.querySelector("message").value;
+    const message_value = document.querySelector("message").value;
     localStorage.setItem("message_value", message_value);
 });
 
